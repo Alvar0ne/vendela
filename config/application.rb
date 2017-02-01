70e4ce73774fd4adb34cca6d33efea3e219edea0
Alvar0ne <alvaro_utem@hotmail.com>
2017-02-01 14:58:57 -0300

    Heroku3

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Vendela
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
     # -- all .rb files in that directory are automatically loaded.
   end
 end
