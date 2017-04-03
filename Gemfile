84c5b24cfaeec982a9ecac2d4d8ba8ae759f2705
Alvar0ne <alvaro_utem@hotmail.com>
2017-04-02 22:59:26 -0300

    Conitua prueba deploy

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use sqlite3 as the database for Active Record
 gem 'bootstrap-select-wrapper-rails'
-gem 'sqlite3' , group: :development
-gem 'pg' , group: :production
+
+gem 'pg' 
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20170328200546) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comunas", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "region_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["region_id"], name: "index_comunas_on_region_id"
+    t.index ["region_id"], name: "index_comunas_on_region_id", using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -26,10 +29,10 @@ ActiveRecord::Schema.define(version: 20170328200546) do
     t.string   "sluggable_type", limit: 50
     t.string   "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
   end
 
   create_table "publication_attachments", force: :cascade do |t|
@@ -62,10 +65,10 @@ ActiveRecord::Schema.define(version: 20170328200546) do
     t.string   "avatar"
     t.integer  "comuna_id"
     t.integer  "region_id"
-    t.index ["comuna_id"], name: "index_publications_on_comuna_id"
-    t.index ["region_id"], name: "index_publications_on_region_id"
-    t.index ["slug"], name: "index_publications_on_slug", unique: true
-    t.index ["user_id"], name: "index_publications_on_user_id"
+    t.index ["comuna_id"], name: "index_publications_on_comuna_id", using: :btree
+    t.index ["region_id"], name: "index_publications_on_region_id", using: :btree
+    t.index ["slug"], name: "index_publications_on_slug", unique: true, using: :btree
+    t.index ["user_id"], name: "index_publications_on_user_id", using: :btree
   end
 
   create_table "regions", force: :cascade do |t|
@@ -92,8 +95,12 @@ ActiveRecord::Schema.define(version: 20170328200546) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "permission_level",       default: 1
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comunas", "regions"
+  add_foreign_key "publications", "comunas"
+  add_foreign_key "publications", "regions"
+  add_foreign_key "publications", "users"
 end
